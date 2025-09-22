3a361f439e6f1f2833733ba0495c4e6460d1bc8d
saitama <yashjoshi6787@gmail.com>
2025-09-23 05:19:35 +0530

    news now has a carousel

--- features/home/News.tsx
+++ features/home/News.tsx
@@ -2,12 +2,24 @@ import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
+import { Button } from "@/components/ui/button";
 import Container from "@mui/material/Container";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import Image, { StaticImageData } from "next/image";
-import { ReactNode } from "react";
+import { ReactNode , useState} from "react";
 import { NextPage } from "next";
 import newsData from "../../data/NewsData";
+import { Grid } from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+// import { CardSim } from "lucide-react";
 
 interface NewsProps {
   img?: StaticImageData;
@@ -31,7 +43,7 @@ const News: NextPage = () => {
           <Box
             sx={{
               height: "208px",
-              width: "262px",
+              width: "100%",
             }}
           >
             <Image
@@ -94,15 +106,23 @@ const News: NextPage = () => {
       </Stack>
     );
   };
+
+  const [curr , setCurr] = useState(0);
+  const len_of_news = newsData.length;
+  console.log(newsData[curr])
+
+  const handlePrev = () =>{
+    setCurr((curr)=> (curr-1+len_of_news)%len_of_news)
+  }
+
+  const handleNext = () =>{
+    setCurr((curr)=> (curr+1+len_of_news)%len_of_news)
+  }
+
   return (
     <>
-      <Container
-        maxWidth="lg"
-        sx={{
-          mt: "120px",
-        }}
-      >
-        <Box>
+    <Container>
+              <Box sx={{mb:"44px"}}>
           <Typography
             variant="h1"
             fontFamily={"Playfair Display"}
@@ -116,94 +136,55 @@ const News: NextPage = () => {
             News
           </Typography>
         </Box>
-        <Box
-          sx={{
-            mt: "44px",
-          }}
-        >
-          <Grid2 container spacing={1.5} justifyContent="center">
-            <Grid2 xs={2.99} display="flex" flexDirection={"column"} gap={2}>
-              <Item
-                img={newsData[0].img}
-                title={newsData[0].title}
-                authorImg={newsData[0].authImg}
-                author={newsData[0].authName}
-              >
-                {newsData[0].text}
-              </Item>
-              {<Divider orientation="horizontal" />}
-              {<Item
-                img={newsData[1].img}
-                title={newsData[1].title}
-                authorImg={newsData[1].authImg}
-                author={newsData[1].authName}
-              >
-                {newsData[1].text}
-              </Item>}
-            </Grid2>
-            {<Divider orientation="vertical" flexItem />}
-            <Grid2 xs={2.99} display="flex" flexDirection={"column"} gap={2}>
-              <Item
-                img={newsData[2].img}
-                title={newsData[2].title}
-                authorImg={newsData[2].authImg}
-                author={newsData[2].authName}
-              >
-                {newsData[2].text}
-              </Item>
-              <Divider orientation="horizontal" />
-              <Item
-                img={newsData[3].img}
-                title={newsData[3].title}
-                authorImg={newsData[3].authImg}
-                author={newsData[3].authName}
-              >
-                {newsData[3].text}
-              </Item>
-            </Grid2>
-            <Divider orientation="vertical" flexItem />
-            <Grid2 xs={2.99} display="flex" flexDirection={"column"} gap={2}>
-              <Item
-                img={newsData[4].img}
-                title={newsData[4].title}
-                authorImg={newsData[4].authImg}
-                author={newsData[4].authName}
-              >
-                {newsData[4].text}
-              </Item>
-              <Divider orientation="horizontal" />
-              <Item
-                img={newsData[5].img}
-                title={newsData[5].title}
-                authorImg={newsData[5].authImg}
-                author={newsData[5].authName}
-              >
-                {newsData[5].text}
-              </Item>
-            </Grid2>
-            <Divider orientation="vertical" flexItem />
-            <Grid2 xs={2.99} display="flex" flexDirection={"column"} gap={2}>
-              <Item
-                img={newsData[6].img}
-                title={newsData[6].title}
-                authorImg={newsData[6].authImg}
-                author={newsData[6].authName}
-              >
-                {newsData[6].text}
-              </Item>
-              <Divider orientation="horizontal" />
-              <Item
-                img={newsData[7].img}
-                title={newsData[7].title}
-                authorImg={newsData[7].authImg}
-                author={newsData[7].authName}
-              >
-                {newsData[7].text}
-              </Item>
-            </Grid2>
-          </Grid2>
+   
+    <Container sx={{alignItems:"center" , height:500}}>
+      <Box sx={{ width:"100%" , display:"flex" , height:"100%" }}>
+        <Box sx={{
+          width:"5%",
+          display:"flex",
+          alignItems:"center",
+          justifyContent:"center"
+        }} 
+
+        ><Button onClick={handlePrev}><ArrowBackIcon></ArrowBackIcon></Button>
         </Box>
+
+
+        <Box sx={{width:"90%" , display:"flex" , padding:"5px" , alignItems:"center",
+          justifyContent:"center"}}
+        >
+          <Box sx={{width:"50%" , padding:"10px" ,}}>
+            <Box sx={{
+                    width: "100%",     
+                    height: 500,       
+                    objectFit: "cover",
+                    borderRadius: 2,   
+                  }}
+                  component="img" src=
+          {newsData[curr].img.src}>
+            </Box>
+          </Box>
+          <Box sx={{width:"50%" , padding:"10px"}}>
+            <Box sx={{maxheight:"80%", fontSize:"1.3rem" , height: 500 , padding:2}}>{newsData[curr].text}</Box>
+            <Box sx={{minHeight:"20%" , padding:2 , fontSize:"1.1rem" , display:"flex"}}>
+              <Box sx={{minWidth:"70%"}}></Box>
+              <Box sx={{display:"flex" , alignItems:"center" }}>
+                  <Box component="img" src={newsData[curr].authImg.src} sx={{height:40 , width:40, borderRadius:10 , marginRight:1 }}></Box>
+                  <Box>{newsData[curr].authName}</Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+
+
+        <Box sx={{width:"5%" ,display:"flex" ,alignItems:"center", justifyContent:"center"}}>
+          <Button onClick={handleNext}><ArrowForwardIcon></ArrowForwardIcon></Button>
+        </Box>
+
+
+      </Box>
       </Container>
+       </Container>
     </>
   );
 };
